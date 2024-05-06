d14db25796cae836b4f9991fbdf08ad0a22c22ce
unknown <anshika52025@gmail.com>
2024-05-06 23:35:50 +0530

    SPLIT-APP

--- App.js
+++ App.js
@@ -37,7 +37,7 @@ app.all('*', (req, res) => {
       })
 })
 
-const port = process.env.PORT || 3002
+const port = process.env.PORT || 3001
 app.listen(port, (err) => {
     console.log(`Server started in PORT | ${port}`)
     logger.info(`Server started in PORT | ${port}`)
